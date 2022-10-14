b26ec98e30900280adf76b55e3c290daa6471adf
Tomasz Kielar <tkielar@pgs-soft.com>
2022-10-14 13:14:02 +0200

    Support for computed connection details

--- internal/core/datasource/datasourceWrapper.go
+++ internal/core/datasource/datasourceWrapper.go
@@ -3,7 +3,7 @@ package datasource
 import (
 	"context"
 	"fmt"
-	"github.com/PGSSoft/terraform-provider-mssql/internal/sql"
+	"github.com/PGSSoft/terraform-provider-mssql/internal/core"
 	"github.com/PGSSoft/terraform-provider-mssql/internal/utils"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
@@ -17,8 +17,8 @@ func NewDataSource[T any](d DataSource[T]) func() datasource.DataSourceWithConfi
 }
 
 type dataSourceWrapper[T any] struct {
-	d    DataSource[T]
-	conn sql.Connection
+	d   DataSource[T]
+	ctx core.ResourceContext
 }
 
 func (d *dataSourceWrapper[T]) Metadata(_ context.Context, req datasource.MetadataRequest, resp *datasource.MetadataResponse) {
@@ -30,16 +30,16 @@ func (d *dataSourceWrapper[T]) Configure(_ context.Context, req datasource.Confi
 		return
 	}
 
-	db, ok := req.ProviderData.(sql.Connection)
+	resourceCtx, ok := req.ProviderData.(core.ResourceContext)
 
 	if !ok {
 		resp.Diagnostics.AddError(
 			"Unexpected data source configure type",
-			fmt.Sprintf("Expected sql.Connection, got: %T. Please report this issue to the provider developers.", req.ProviderData))
+			fmt.Sprintf("Expected ResourceContext, got: %T. Please report this issue to the provider developers.", req.ProviderData))
 		return
 	}
 
-	d.conn = db
+	d.ctx = resourceCtx
 }
 
 func (d *dataSourceWrapper[T]) GetSchema(ctx context.Context) (tfsdk.Schema, diag.Diagnostics) {
@@ -50,10 +50,10 @@ func (d *dataSourceWrapper[T]) GetSchema(ctx context.Context) (tfsdk.Schema, dia
 func (d *dataSourceWrapper[T]) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
 	ctx = utils.WithDiagnostics(ctx, &resp.Diagnostics)
 
-	request := ReadRequest[T]{
-		Conn: d.conn,
-	}
-	request.monad = utils.StopOnError(ctx).Then(func() { request.Config = utils.GetData[T](ctx, req.Config) })
+	request := ReadRequest[T]{}
+	request.monad = utils.StopOnError(ctx).
+		Then(func() { request.Conn = d.ctx.ConnFactory(ctx) }).
+		Then(func() { request.Config = utils.GetData[T](ctx, req.Config) })
 
 	response := ReadResponse[T]{}
 	d.d.Read(ctx, request, &response)
