d496a818d757252313e4123ecd5e08fb86693a2b
Tomasz Kielar <tkielar@pgs-soft.com>
2022-10-28 12:51:14 +0200

    Fixed use of deprecated attributes

--- internal/services/databasePermission/resource.go
+++ internal/services/databasePermission/resource.go
@@ -19,15 +19,15 @@ type resourceData struct {
 }
 
 func (d resourceData) withPermission(perm sql.DatabasePermission) resourceData {
-	d.Permission = types.String{Value: perm.Name}
-	d.WithGrantOption = types.Bool{Value: perm.WithGrantOption}
+	d.Permission = types.StringValue(perm.Name)
+	d.WithGrantOption = types.BoolValue(perm.WithGrantOption)
 	return d
 }
 
 func (d resourceData) toPermission() sql.DatabasePermission {
 	return sql.DatabasePermission{
-		Name:            d.Permission.Value,
-		WithGrantOption: d.WithGrantOption.Value,
+		Name:            d.Permission.ValueString(),
+		WithGrantOption: d.WithGrantOption.ValueBool(),
 	}
 }
 
@@ -37,7 +37,7 @@ func (r res) GetName() string {
 	return "database_permission"
 }
 
-func (r res) GetSchema(ctx context.Context) tfsdk.Schema {
+func (r res) GetSchema(context.Context) tfsdk.Schema {
 	return tfsdk.Schema{
 		MarkdownDescription: "Grants database-level permission.",
 		Attributes: map[string]tfsdk.Attribute{
@@ -62,7 +62,7 @@ func (r res) Read(ctx context.Context, req resource.ReadRequest[resourceData], r
 	req.
 		Then(func() { permissions = db.GetPermissions(ctx, principalId.ObjectId) }).
 		Then(func() {
-			req.State.PrincipalId = types.String{Value: principalId.String()}
+			req.State.PrincipalId = types.StringValue(principalId.String())
 
 			if perm, ok := permissions[permission]; ok {
 				resp.SetState(req.State.withPermission(perm))
@@ -78,14 +78,13 @@ func (r res) Create(ctx context.Context, req resource.CreateRequest[resourceData
 
 	req.
 		Then(func() {
-			principalId = common.ParseDbObjectId[sql.GenericDatabasePrincipalId](ctx, req.Plan.PrincipalId.Value)
+			principalId = common.ParseDbObjectId[sql.GenericDatabasePrincipalId](ctx, req.Plan.PrincipalId.ValueString())
 		}).
 		Then(func() { db = sql.GetDatabase(ctx, req.Conn, principalId.DbId) }).
 		Then(func() { db.GrantPermission(ctx, principalId.ObjectId, req.Plan.toPermission()) }).
 		Then(func() {
-			req.Plan.WithGrantOption.Unknown = false
-			req.Plan.WithGrantOption.Null = false
-			req.Plan.Id = types.String{Value: fmt.Sprintf("%v/%s", principalId, req.Plan.Permission.Value)}
+			req.Plan.WithGrantOption = types.BoolValue(req.Plan.WithGrantOption.ValueBool())
+			req.Plan.Id = types.StringValue(fmt.Sprintf("%v/%s", principalId, req.Plan.Permission.ValueString()))
 
 			resp.State = req.Plan
 		})
@@ -99,16 +98,16 @@ func (r res) Update(ctx context.Context, req resource.UpdateRequest[resourceData
 		Then(func() { resp.State = req.Plan.withPermission(req.Plan.toPermission()) })
 }
 
-func (r res) Delete(ctx context.Context, req resource.DeleteRequest[resourceData], resp *resource.DeleteResponse[resourceData]) {
+func (r res) Delete(ctx context.Context, req resource.DeleteRequest[resourceData], _ *resource.DeleteResponse[resourceData]) {
 	db, principalId, permission := r.parseInputs(ctx, req.Conn, req.State)
 
 	req.Then(func() { db.RevokePermission(ctx, principalId.ObjectId, permission) })
 }
 
 func (r res) parseInputs(ctx context.Context, conn sql.Connection, data resourceData) (sql.Database, common.DbObjectId[sql.GenericDatabasePrincipalId], string) {
-	parts := strings.Split(data.Id.Value, "/")
+	parts := strings.Split(data.Id.ValueString(), "/")
 	permission := parts[len(parts)-1]
-	principalId := common.ParseDbObjectId[sql.GenericDatabasePrincipalId](ctx, data.Id.Value[:len(data.Id.Value)-len(permission)-1])
+	principalId := common.ParseDbObjectId[sql.GenericDatabasePrincipalId](ctx, data.Id.ValueString()[:len(data.Id.ValueString())-len(permission)-1])
 
 	return sql.GetDatabase(ctx, conn, principalId.DbId), principalId, permission
 }
